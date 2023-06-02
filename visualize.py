f9ffc8125fa6dd86b4f45ee6123d1c35e37be35a
saeedahmadicp <saeedahmad.icp@gmail.com>
2023-06-02 09:59:07 +0900

    model predictions for best cases

--- visualize.py
+++ visualize.py
@@ -75,8 +75,8 @@ def visualize(config, data_dict, dataset_dir, testONT1ce=True):
     print(overall_performance[0].keys()) 
     
     ## visualize the volumes for worst 15 samples 
-    for idx in range(15):
-        sample = overall_performance[-idx + 1]["sample"]
+    for idx in range(10):
+        sample = overall_performance[idx]["sample"]
         print(sample)
         x, y = test_ds[sample]
         x = x.unsqueeze(0)
